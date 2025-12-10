173f7841d7d01f67d8474f253181c52ef61524fb
Bárbara Lampert <lampertbarbara97@gmail.com>
2025-12-10 14:25:03 -0300

    "feat(ml): padronizar target e separar features/target para treinamento"

--- train.py
+++ train.py
@@ -32,3 +32,35 @@ data = data[columns].copy()
 data.rename(columns=features, inplace=True)
 
 data 
+
+# %%
+
+# ver as classes da variável target
+data[target].unique()
+
+
+depara_salario = {
+    'Menos de R$ 1.000/mês':'01 - Menos de R$ 1.000/mês',
+    'de R$ 1.001/mês a R$ 2.000/mês':'02 - R$ 1.001/mês a R$ 2.000/mês',
+    'de R$ 2.001/mês a R$ 3.000/mês':'03 - R$ 2.001/mês a R$ 3.000/mês',
+    'de R$ 3.001/mês a R$ 4.000/mês':'04 - R$ 3.001/mês a R$ 4.000/mês',
+    'de R$ 4.001/mês a R$ 6.000/mês':'05 - R$ 4.001/mês a R$ 6.000/mês',
+    'de R$ 6.001/mês a R$ 8.000/mês':'06 - R$ 6.001/mês a R$ 8.000/mês',
+    'de R$ 8.001/mês a R$ 12.000/mês':'07 - R$ 8.001/mês a R$ 12.000/mês',
+    'de R$ 12.001/mês a R$ 16.000/mês':'08 - R$ 12.001/mês a R$ 16.000/mês',
+    'de R$ 16.001/mês a R$ 20.000/mês':'09 - R$ 16.001/mês a R$ 20.000/mês',
+    'de R$ 20.001/mês a R$ 25.000/mês':'10 - R$ 20.001/mês a R$ 25.000/mês',
+    'de R$ 25.001/mês a R$ 30.000/mês':'11 - R$ 25.001/mês a R$ 30.000/mês',
+    'de R$ 30.001/mês a R$ 40.000/mês':'12 - R$ 30.001/mês a R$ 40.000/mês',
+    'Acima de R$ 40.001/mês':'13 - Acima de R$ 40.001/mês',
+}
+
+data[target] = data[target].replace(depara_salario)
+
+data
+# %%
+
+x = data[features.values()]
+y = data[target]
+y
+# %%
